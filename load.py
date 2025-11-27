34a953e03f7345cd5f55de2eb70f3ae02a5dfd56
Mufliha Dawood <ms2176@hw.ac.uk>
2025-11-27 20:23:38 +0400

    Add UTF-8 encoding to file opening in load_data function

--- load.py
+++ load.py
@@ -1,7 +1,7 @@
 import json
 
 def load_data(file_path):
-    with open(file_path, "r") as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         for line in f:
             if line != '\n':
                 yield json.loads(line)
